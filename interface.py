926edd55c0688283af95e9252463b7fa771a9ab3
uwase <byusagisele@gmail.com>
2020-11-16 15:16:57 +0200

    added something on interface

--- interface.py
+++ interface.py
@@ -5,7 +5,7 @@ from passlock import User, Credentials ;
 
 def create_new_user(username,password):
     '''
-    Function to create new user with a username and password
+    Function to create  a new user with a username and password
     '''
     new_user = User(username,password)
     return new_user
