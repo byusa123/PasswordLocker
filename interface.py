66553f52a015707fbe0928317ef873d794e404e4
uwase <byusagisele@gmail.com>
2020-11-16 15:33:30 +0200

    added something on interface

--- interface.py
+++ interface.py
@@ -79,7 +79,7 @@ def copy_password(account):
 
 
 def passlocker():
-    print("Hello Welcome to your Accounts Password Store...\n Please enter one of the following to proceed.\n CA ---  Create New Account  \n LI ---  Have An Account  \n")
+    print(" Welcome to your Accounts Password Store...\n Please enter one of the following to proceed.\n CA ---  Create New Account  \n LI ---  Have An Account  \n")
     short_code=input("").lower().strip()
     if short_code == "ca":
         print("Sign Up")
